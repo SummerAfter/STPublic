5053080892f9be0cafffe057d4299b2e03a624fe
renchaoyue <130220604@qq.com>
2021-01-27 15:25:49 +0800

    fix:test

---
+++ Classes/UILabel+ST.h
@@ -0,0 +1,16 @@
+//
+//  UILabel+ST.h
+//  STPublic
+//
+//  Created by Showtime on 2021/1/27.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UILabel (ST)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/UILabel+ST.m
@@ -0,0 +1,12 @@
+//
+//  UILabel+ST.m
+//  STPublic
+//
+//  Created by Showtime on 2021/1/27.
+//
+
+#import "UILabel+ST.h"
+
+@implementation UILabel (ST)
+
+@end
