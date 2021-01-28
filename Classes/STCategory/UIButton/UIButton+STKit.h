0f7df964900bac309c219f67fc656f5082726df0
renchaoyue <130220604@qq.com>
2021-01-28 13:54:13 +0800

    feat:添加UIButton拓展类

---
+++ Classes/STCategory/UIButton/UIButton+STKit.h
@@ -0,0 +1,18 @@
+//
+//  UIButton+STKit.h
+//  STPublic
+//
+//  Created by Showtime on 2021/1/27.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIButton (STKit)
+
+- (void)startTime:(NSInteger )timeout title:(NSString *)tittle waitTittle:(NSString *)waitTittle;
+
+@end
+
+NS_ASSUME_NONNULL_END
